dde72df9d4e6a2e77032527111fe2a8100b42e85
Jennifer Molony | NoSh. <jennifer.molony@ucdconnect.ie>
2025-11-02 18:47:18 +0100

    Fix Duplicate LanguageProvider Import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { LanguageProvider } from './contexts/LanguageContext';
-import { LanguageProvider } from './contexts/LanguageContext';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
